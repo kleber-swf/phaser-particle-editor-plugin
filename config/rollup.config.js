93f8d6b1c09b587dab5ac4e21fbfa6118bb0f9a3
Kleber Silva <kleber.swf@gmail.com>
2021-07-27 16:58:26 -0300

    added declarations

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -4,29 +4,55 @@ import commonjs from '@rollup/plugin-commonjs';
 
 const pkg = require('../package.json');
 
-export default {
-	input: pkg.input,
-	external: [
-		...Object.keys(pkg.dependencies || {}),
-		...Object.keys(pkg.peerDependencies || {}),
-	],
-	treeshake: false,
-	plugins: [
-		commonjs(),
-		typescript({
-			tsconfig: './tsconfig.json',
-		}),
-		terser()
-	],
-	output: [
-		{
-			file: './dist/phaser-particle-editor-plugin.min.js',
-			format: 'umd',
-			name: 'ParticleEditor',
-			exports: 'named',
-			globals: {
-				'phaser-ce': 'Phaser',
-			},
-		}
-	],
-};
+export default [
+	{
+		input: pkg.input,
+		external: [
+			...Object.keys(pkg.dependencies || {}),
+			...Object.keys(pkg.peerDependencies || {}),
+		],
+		treeshake: false,
+		plugins: [
+			commonjs(),
+			typescript({
+				tsconfig: './tsconfig.json',
+			}),
+			terser()
+		],
+		output: [
+			{
+				file: './dist/phaser-particle-editor-plugin.min.js',
+				format: 'umd',
+				name: 'ParticleEditor',
+				exports: 'named',
+				globals: {
+					'phaser-ce': 'Phaser',
+				},
+			}
+		],
+	},
+	{
+		input: pkg.input,
+		external: [
+			...Object.keys(pkg.dependencies || {}),
+			...Object.keys(pkg.peerDependencies || {}),
+		],
+		treeshake: false,
+		plugins: [
+			commonjs(),
+			typescript({
+				tsconfig: 'tsconfig.json',
+				rootDir: './src',
+				outDir: './dist',
+				declaration: true,
+				emitDeclarationOnly: true,
+				sourceMap: true,
+			}),
+		],
+		output: {
+			dir: './dist',
+			esModule: false,
+			sourcemap: true,
+		},
+	}
+];
