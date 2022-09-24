ba2101549ac02a6f71081b5b6f4a55192739260c
Kleber Silva <kleber@jogoglobal.com>
2022-09-24 14:18:17 -0300

    Particle collition made optional by a constructor parameter

--- src/particle-effect.ts
+++ src/particle-effect.ts
@@ -4,54 +4,45 @@ import { createImageFromBitmapData } from './utils';
 
 export class ParticleEffect extends Phaser.Group {
 	private readonly _emitersMap: Map<string, ExtendedEmitter>;
-	private readonly _collidebleEmitters: Set<ExtendedEmitter>;
-	private readonly _arcade: Phaser.Physics.Arcade;
+	private _collidebleEmitters: Set<ExtendedEmitter>;
+	private _arcade: Phaser.Physics.Arcade;
 	private _emitX: number;
 	private _emitY: number;
 
-	constructor(game: Phaser.Game, particleData: ParticleData, x: number, y: number) {
+	constructor(game: Phaser.Game, particleData: ParticleData, x: number, y: number, supportCollision = true) {
 		super(game);
 		this.x = x;
 		this.y = y;
 		this._emitersMap = new Map();
-		this._collidebleEmitters = new Set();
 		this._emitX = 0;
 		this._emitY = 0;
-		this._arcade = this.game.physics.arcade;
 		this._initEmitters(particleData);
+		if (supportCollision) {
+			this._arcade = this.game.physics.arcade;
+			this._collidebleEmitters = new Set();
+			this.update = this._updateWithCollision.bind(this);
+		}
 	}
 
 	get emitX() { return this._emitX; }
 
 	set emitX(x) {
 		this._emitX = x;
-		Object.values(this._emitersMap).forEach((emitter: ExtendedEmitter) => emitter.emitX = emitter.properties.emitX + this.emitX);
-		// eslint-disable-next-line no-unused-vars
-		// for (const [key, emitter] of this._emitersMap) {
-		// 	emitter.emitX = emitter.properties.emitX + this.emitX;
-		// }
+		Object.values(this._emitersMap)
+			.forEach((emitter: ExtendedEmitter) => emitter.emitX = emitter.properties.emitX + this.emitX);
 	}
 
 	get emitY() { return this._emitY; }
 
 	set emitY(y) {
 		this._emitY = y;
-		Object.values(this._emitersMap).forEach((emitter: ExtendedEmitter) => emitter.emitY = emitter.properties.emitY + this.emitY);
-		// for (const [key, emitter] of this._emitersMap) {
-		// 	emitter.emitY = emitter.properties.emitY + this.emitY;
-		// }
+		Object.values(this._emitersMap)
+			.forEach((emitter: ExtendedEmitter) => emitter.emitY = emitter.properties.emitY + this.emitY);
 	}
 
 	private _initEmitters(particleData: ParticleData) {
 		const emitters = particleData.emitters;
 		Object.entries(emitters).forEach((e) => this.addEmitter(e[0], e[1]));
-		// for (const emitterName in emitters) {
-		// 	if (!emitters.hasOwnProperty(emitterName)) {
-		// 		continue;
-		// 	}
-		// 	const emitterProperties = emitters[emitterName];
-		// 	this.addEmitter(emitterName, emitterProperties);
-		// }
 	}
 
 	private _createEmitter(name: string, properties: EmitterProperties) {
@@ -91,20 +82,14 @@ export class ParticleEffect extends Phaser.Group {
 		}
 	}
 
-	public update() {
+	private _updateWithCollision() {
 		super.update();
 		Object.keys(this._collidebleEmitters).forEach(k => this._arcade.collide(k));
-		// for (const emitter of this._collidebleEmitters) {
-		// 	this._arcade.collide(emitter);
-		// }
 	}
 
 	public emit() {
-		Object.values(this._emitersMap).forEach((emitter: ExtendedEmitter) => this._emit(emitter, emitter.properties));
-		// eslint-disable-next-line no-unused-vars
-		// for (const [key, emitter] of this._emitersMap) {
-		// 	this._emit(emitter, emitter.properties);
-		// }
+		Object.values(this._emitersMap)
+			.forEach((emitter: ExtendedEmitter) => this._emit(emitter, emitter.properties));
 	}
 
 	private _onEmitterImageUpdate(name: string, properties: EmitterProperties) {
@@ -138,10 +123,12 @@ export class ParticleEffect extends Phaser.Group {
 				if (autoEmit) {
 					this._emit(emitter, properties);
 				}
-				if (properties.collide || properties.collideWorldBounds) {
-					this._collidebleEmitters.add(emitter);
-				} else {
-					this._collidebleEmitters.delete(emitter);
+				if (this._collidebleEmitters) {
+					if (properties.collide || properties.collideWorldBounds) {
+						this._collidebleEmitters.add(emitter);
+					} else {
+						this._collidebleEmitters.delete(emitter);
+					}
 				}
 			},
 			null,
